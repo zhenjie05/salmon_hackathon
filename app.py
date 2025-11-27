578abd338f834bc47904b123056edb8990a2908f
Lee Zhen Jie <zhenjie.lee05@gmail.com>
2025-11-27 22:17:31 +0800

    Integrate Streamlit with JamAI backend

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+import streamlit as st
+from jamai_backend import ask_jamai
+
+st.title("JamAI Backend + Streamlit Frontend")
+
+prompt = st.text_input("Ask something:")
+if st.button("Send"):
+    answer = ask_jamai(prompt)
+    st.write(answer)
 
