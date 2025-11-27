2e8de19e6f5bbed435649eb7a8d9c1fbb26cfd6e
Lee Zhen Jie <zhenjie.lee05@gmail.com>
2025-11-27 22:16:57 +0800

    Update jamai_backend.py

--- jamai_backend.py
+++ jamai_backend.py
@@ -1,6 +1,6 @@
 from jamai import JamAI
 
 def ask_jamai(prompt):
-    jamai = JamAI(api_key="YOUR_JAMAI_KEY")
+    jamai = JamAI(api_key="jamai_pat_49188da586bc90838807d00bb0d2ce9b585633f7d9ab07ca")
     return jamai.chat(prompt)
 
